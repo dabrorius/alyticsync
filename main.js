15dcc97d9ca08b9ac0d6803d902608ac417de92c
Filip Defar <filip.defar@toptal.com>
2018-09-07 15:43:15 +0200

    Handle getting auth token better

--- main.js
+++ main.js
@@ -48,8 +48,8 @@ function login() {
 
 // Read the files from the server and write them locally
 function pull() {
-  const authContent = fs.readFileSync('.alyticstoken', "utf-8", (err) => console.log(err));
-  axios.get('https://alytic.io/api/v2/decks/7569/cards/112207', {headers: {Authorization: authContent}})
+  const authToken = getAuthToken();
+  axios.get('https://alytic.io/api/v2/decks/7569/cards/112207', {headers: {Authorization: authToken}})
     .then(function (response) {
       const {graphic_script, css, queries} = response.data.overrides;
       fs.writeFile('./style.css', css, (err) => console.log(err));
@@ -61,10 +61,10 @@ function pull() {
 
 // Read local files and write them to server
 function push() {
-  const authContent = fs.readFileSync('.alyticstoken', "utf-8", (err) => console.log(err));
+  const authToken = getAuthToken();
   const css = fs.readFileSync('./style.css', "utf-8", (err) => console.log(err));
   axios.patch('https://alytic.io/api/v2/decks/7569/cards/112207', {overrides: {css}}, {
-      headers: {Authorization: authContent, 'Content-Type': 'application/json'}
+      headers: {Authorization: authToken, 'Content-Type': 'application/json'}
     })
     .then(function (response) {
       console.log('Done!');
@@ -72,4 +72,13 @@ function push() {
     .catch(function (error) {
       console.log(error);
     });
+}
+
+function getAuthToken() {
+  try {
+    return fs.readFileSync('.alyticstoken', "utf-8");
+  } catch {
+    console.log('Please login first!');
+    process.exit(1);
+  }
 }
