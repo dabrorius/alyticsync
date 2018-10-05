9e4c2b623d91c29387dc6d9f22524c883920b2a2
Filip Defar <filip.defar@toptal.com>
2018-10-05 14:59:44 +0200

    Wrtie list os scripts to config file when pulling

--- src/commands/pull/main.js
+++ src/commands/pull/main.js
@@ -5,14 +5,16 @@ const splitScript = require("./splitScript.js");
  * from the API
  * and write them to local files.
  */
-function pull(fetchPromise, writeFile, log) {
+function pull(fetchPromise, writeFile, storeConfig, log) {
   const errorHandler = err => console.log(err);
   fetchPromise.then(function(response) {
     const { graphic_script, css, queries } = response.data.overrides;
     writeFile("./style.css", css, errorHandler);
-    splitScript(graphic_script).forEach(script => {
+    const scripts = splitScript(graphic_script);
+    scripts.forEach(script => {
       writeFile(`./${script.file}`, script.content, errorHandler);
     });
+    storeConfig({ scripts: scripts.map(s => s.file) });
     writeFile("./queries.json", JSON.stringify(queries, null, 2), errorHandler);
     log("Changes have been puled from alytic.io");
   });
